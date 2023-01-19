00fab7a90d93042a654071084d749b7953cc7cf1
ziz <azizazu0934@gmail.com>
2023-01-19 20:58:17 +0300

    How Render Lists

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React from 'react';
 
-function getTitle(title) {
-  return title;
-}
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>
-        Hello {getTitle("React")}
-      </h1>
+      <h1>My Hacker Stories</h1>
 
-      <label htmlFor='search'>Search</label>
+      <label htmlFor='search'>Search: </label>
       <input id='search' type='text'></input>
+
+      <hr />
+
+      {/* render the list here */}
     </div>
   );
 }
