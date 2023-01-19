315a8e165993aa82ef735af8017db785ef098fdd
ziz <azizazu0934@gmail.com>
2023-01-19 19:28:28 +0300

    Everything in curly braces in JSX can be used for JavaScript expressions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
-const welcome = {
-  greeting: "Hey",
-  title: "React",
-};
+function getTitle(title) {
+  return title;
+}
 
 function App() {
   return (
     <div>
       <h1>
-        {welcome.greeting} {welcome.title}
+        Hello {getTitle("React")}
       </h1>
 
       <label htmlFor='search'>Search</label>
