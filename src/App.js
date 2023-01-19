6b821120c1ccf08d21edfb9232c452218af6250c
ziz <azizazu0934@gmail.com>
2023-01-19 19:07:57 +0300

    Add Input Element and label element with htmlFor, label and id attributes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const title = "Abdulaziz";
+const title = "React";
 
 function App() {
   return (
     <div>
       <h1>Hello {title}</h1>
+
+      <label htmlFor='search'>Search</label>
+      <input id='search' type='text'></input>
     </div>
   );
 }
