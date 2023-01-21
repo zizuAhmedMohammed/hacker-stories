c9c6d9945373ccf4bbe42b44095deb1d03382464
ziz <azizazu0934@gmail.com>
2023-01-21 19:17:39 +0300

    Create My first react component

--- src/App.js
+++ src/App.js
@@ -19,6 +19,20 @@ const list = [
   },
 ];
 
+function List() {
+  return (list.map(function(item) {
+      return (
+        <div key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </div>
+      );
+    }));      
+}
 function App() {
   return (
     <div>
@@ -29,18 +43,7 @@ function App() {
 
       <hr />
 
-      {list.map(function(item) {
-        return (
-        <div key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-        </div>
-        )
-      })}
+      <List />
     </div>
   );
 }
