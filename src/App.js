8f140588f7af3350fbd0a6a3b1c5c118acd30282
ziz <azizazu0934@gmail.com>
2023-01-22 23:28:27 +0300

    Handle Change event of the input field

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
 
-const App = () => (
-  <div>
+const App = () => {
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  return (
+    <div>
     <h1>My Hacker Stories</h1>
 
     <label htmlFor='search'>Search: </label>
-    <input id='search' type='text'></input>
+    <input id='search' type='text' onChange={handleChange}/>
 
     <hr />
 
     <List />
-
   </div>
-);
+  );
+};
 
 const list = [
   {
