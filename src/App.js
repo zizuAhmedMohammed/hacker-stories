cacc0b58e28617ead9411f4feae3a2a38170cbca
ziz <azizazu0934@gmail.com>
2023-01-24 23:37:02 +0300

    Lifting State in React

--- src/App.js
+++ src/App.js
@@ -19,40 +19,40 @@ const App = () => {
       objectID: 1,
     },
   ];
-  
+  const [searchTerm, setSearchTerm] = React.useState('')
+
   const handleSearch = event => {
-    console.log(event.target.value);
-  }
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedStories = stories.filter( story => 
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
     <h1>My Hacker Stories</h1>
 
     <Search onSearch={handleSearch}/>
+    {/* <p>Search Keyword: <strong>{searchTerm}</strong></p> */}
 
     <hr />
     
-    <List list={stories}/>
+    <List list={searchedStories}/>
   </div>
   );
 };
 
-const Search = props => {  
-  const [searchTerm, setSearchTerm] = React.useState('');
-  
-  const handleChange = event => {
-    setSearchTerm(event.target.value);
-    props.onSearch(event);
-  };
-  
+const Search = props => { 
+  const {onSearch} = props;  
   return (
   <div>
     <label htmlFor='search'>Search: </label>
-    <input id='search' type='text' onChange={handleChange}/>
-    <p>Searching for <strong>{searchTerm}</strong></p>
+    <input id='search' type='text' onChange={onSearch}/>
   </div>
   );
-}
+};
+ 
 const List = props =>
   props.list.map(item => (
     <div key={item.objectID}>
